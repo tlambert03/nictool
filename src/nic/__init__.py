fbc8a53702ddce9cc2861532d938f77c72ac2d6e
Talley Lambert <talley.lambert@gmail.com>
2022-06-28 08:45:41 -0400

    refactor: remove author

--- src/nic/__init__.py
+++ src/nic/__init__.py
@@ -6,8 +6,6 @@ try:
     __version__ = version("nic")
 except PackageNotFoundError:
     __version__ = "uninstalled"
-__author__ = "Talley Lambert"
-__email__ = "talley.lambert@gmail.com"
 
 from ._cleanup import iter_empty_dirs, iter_old_files
 
