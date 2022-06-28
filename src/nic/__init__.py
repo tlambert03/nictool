0cdaedbacffb9d41f4a96ddcc2927c22a98c8e2d
Talley Lambert <talley.lambert@gmail.com>
2022-06-28 10:41:26 -0400

    build: use plain version

--- src/nic/__init__.py
+++ src/nic/__init__.py
@@ -1,15 +1,9 @@
 """Command line tools for NIC@HMS."""
-
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("nic")
-except PackageNotFoundError:
-    __version__ = "uninstalled"
-
 from ._cleanup import iter_empty_dirs, iter_old_files
 
+__version__ = "0.1.0"
 __all__ = [
+    "__version__",
     "iter_old_files",
     "iter_empty_dirs",
 ]
