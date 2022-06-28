ad2e4faf9b10f21732b8c9edaae3ab139ff4f01e
Talley Lambert <talley.lambert@gmail.com>
2022-06-28 05:52:00 -0400

    update cleanup

--- src/nic/_cleanup.py
+++ src/nic/_cleanup.py
@@ -1,46 +1,23 @@
-import os
+from pathlib import Path
 import time
+from typing import Iterator, Union
 
 
-SAFE_WORD = "delete"  # if this word is in the filename, it won't be deleted
+SEC_PER_DAY = 60 * 60 * 24
+TIME = time.time()
 
 
-def file_age_in_days(file: str) -> float:
-    return (time.time() - os.path.getmtime(file)) / (24 * 60 * 60)
+def iter_old_files(
+    directory: Union[str, Path], min_age: int = 30, safe_match: str = "delete"
+) -> Iterator[Path]:
+    for path in Path(directory).glob("**/*"):
+        if path.is_file():
+            days_old = path.stat().st_mtime / SEC_PER_DAY
+            if days_old > min_age and safe_match not in str(path):
+                yield path
 
 
-def delete_old_files(
-    directory: str, age: int = 30, dry: bool = False, safe: str = SAFE_WORD
-) -> int:
-    deleted = 0
-    for root, _, files in os.walk(directory):
-        for F in files:
-            _f = os.path.join(root, F)
-            if file_age_in_days(_f) > age and safe not in _f.lower():
-                print(f'{"NOT " if dry else ""}deleting: {_f}')
-                if not dry:
-                    try:
-                        os.remove(_f)
-                        deleted += 1
-                    except Exception:
-                        print(f"ERROR: could not delete file {_f}")
-
-    return deleted
-
-
-def delete_emptydir(userd: str, dry: bool = False) -> int:
-    # delete all empty directories
-    totdeleted = 0
-    _before = 1
-    while _before != totdeleted:
-        _before = totdeleted
-        for root, _, _ in os.walk(userd):
-            if not os.listdir(root):
-                print(f"{'NOT ' if dry else ''}deleting: {root}")
-                if not dry:
-                    try:
-                        os.rmdir(root)
-                        totdeleted += 1
-                    except Exception:
-                        print(f"ERROR: could not delete folder {root}")
-    return totdeleted
+def iter_empty_dirs(directory: Union[str, Path]) -> Iterator[Path]:
+    for path in Path(directory).glob("**/*"):
+        if path.is_dir() and not list(path.iterdir()):
+            yield path
